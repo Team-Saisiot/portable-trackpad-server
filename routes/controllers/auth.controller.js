0769564e0f8212c47b5517bc74f500d273147f08
comt-mix <comt-mix@gmail.com>
2022-10-16 21:02:15 +0900

    ✨Feat: login정보 MongoDB 저장 확인.

--- routes/controllers/auth.controller.js
+++ routes/controllers/auth.controller.js
@@ -1,3 +1,40 @@
+const { OAuth2Client } = require("google-auth-library");
+const User = require("../../models/User");
+
 exports.googleAuth = async (req, res, next) => {
-  const userToken = req.body.id_token;
+  const client = new OAuth2Client(process.env.EXPO_CLIENT_ID);
+  const { id_token } = req.body;
+  let decoded;
+
+  try {
+    decoded = await client.verifyIdToken({
+      idToken: id_token,
+      audience: process.env.EXPO_CLIENT_ID,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "internal server error",
+    });
+  }
+
+  if (!decoded) {
+    return res.status(401).json({
+      message: "unauthorized",
+    });
+  }
+
+  try {
+    const { name, email } = decoded.payload;
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({ name, email });
+    }
+
+    return res.json({ user });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Login failed. Please try again",
+    });
+  }
 };
